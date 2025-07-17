393cd8da3d14409bb2eb88696c0aa2cddc851817
suhana <suhanakaura01@gmail.com>
2025-07-17 13:51:54 +0530

    register user

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,17 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const connectToDB = require("./db/db");
+const userRoutes = require("./routes/user.routes");
 
 connectToDB();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.send("hello");
 })
+app.use('/users',userRoutes)
 
 module.exports = app;
