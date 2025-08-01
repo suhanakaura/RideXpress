85f8cdb784ad436d314ce8fcdec06b3e03f86dd6
suhana <suhanakaura01@gmail.com>
2025-08-02 00:18:57 +0530

    create locationSearchPanel

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const LocationSearchPanel = () => {
+  // sample array for locations
+  const location = [
+    "24B, near Kapoor's cafe, Himachal Pradesh",
+    "20C, near Kap street, Ludhiana, Punjab",
+    "50B, near Gyan Sagar, Rajpura, Punjab",
+    "17D, near laxmi narayana, Bilaspur, Himachal Pradesh",
+  ];
   return (
     <div>
-      hilo
-    </div>
-  )
-}
+      {location.map(function (loc) {
+        return (
+          <div className="flex items-center justify-start p-3 rounded-2xl border-2 border-gray-100 active:border-black gap-4 my-2">
+            <h2 className="bg-[#eee] flex items-center justify-center h-9 w-11 rounded-full">
+              <i className="ri-map-pin-fill text-xl"></i>
+            </h2>
+            <h4 className="font-medium">
+              {loc}
+            </h4>
+          </div>
+        );
+      })}
+      {/* sample data */}
 
-export default LocationSearchPanel
+      
+    </div>
+  );
+};
+
+export default LocationSearchPanel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,9 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import "remixicon/fonts/remixicon.css";
 import LocationSearchPanel from "../components/LocationSearchPanel";
+import car from "../assets/car2.webp"
+import moto from "../assets/moto.png"
+import auto from "../assets/auto.webp"
 
 const Home = () => {
   const [pickup, setPickup] = useState("");
@@ -39,7 +42,7 @@ const Home = () => {
     [panelOpen]
   );
   return (
-    <div className="h-screen relative">
+    <div className="h-screen relative overflow-hidden">
       <h1 className="absolute left-5 top-5 text-2xl font-semibold">
         RideXpress
       </h1>
@@ -57,13 +60,13 @@ const Home = () => {
           >
             <i className="ri-arrow-down-wide-line"></i>
           </h5>
-          <h4 className="text-2xl font-semibold mb-3">Find a Trip</h4>
+          <h4 className="text-2xl font-semibold mb-5 mt-3">Find a Trip</h4>
           <form
             onSubmit={(e) => {
               submitHandler(e);
             }}
           >
-            <div className="line absolute h-16 w-1 top-[40%] left-10 bg-gray-900"></div>
+            <div className="line absolute h-16 w-1 top-[52%] left-10 bg-gray-900"></div>
             <input
               className="bg-[#eee] px-12 py-2 text-base rounded-lg mb-3 w-full"
               type="text"
@@ -92,6 +95,36 @@ const Home = () => {
         </div>
         <div ref={panelRef} className="h-[0%] bg-white">
           <LocationSearchPanel />
+        </div>
+      </div>
+      <div className="fixed w-full px-3 py-6 z-10 bottom-0 bg-white translate-y-full">
+        <h3 className="text-2xl font-semibold mb-5">Choose a vehicle</h3>
+        <div className="flex w-full border-2 border-gray-100 active:border-black mb-2 rounded-xl p-3 items-center justify-between">
+          <img className="h-15 bg-transparent" src={car} alt="" />
+          <div className="w-1/2 ml-4">
+            <h4 className="font-medium text-lg">XpressCar<span><i className="ri-user-fill"></i>4</span></h4>
+            <h5 className="font-medium text-sm">2 mins away</h5>
+            <p className="font-normal text-xs text-gray-600">Affordable, secure rides</p>
+          </div>
+          <div className="text-xl font-semibold">$193.20</div>
+        </div>
+        <div className="flex w-full border-2 border-gray-100 active:border-black mb-2 rounded-xl p-3 items-center justify-between">
+          <img className="h-15 bg-transparent" src={moto} alt="" />
+          <div className="w-1/2 ml-10">
+            <h4 className="font-medium text-lg">XpressMoto<span><i className="ri-user-fill"></i>2</span></h4>
+            <h5 className="font-medium text-sm">3 mins away</h5>
+            <p className="font-normal text-xs text-gray-600">Affordable, secure rides</p>
+          </div>
+          <div className="text-xl font-semibold">$60.90</div>
+        </div>
+        <div className="flex w-full mb-2 border-2 border-gray-100 active:border-black rounded-xl p-3 items-center justify-between">
+          <img className="h-15 bg-transparent" src={auto} alt="" />
+          <div className="w-1/2 ml-8">
+            <h4 className="font-medium text-lg">XpressAuto<span><i className="ri-user-fill"></i>3</span></h4>
+            <h5 className="font-medium text-sm">5 mins away</h5>
+            <p className="font-normal text-xs text-gray-600">Affordable, secure rides</p>
+          </div>
+          <div className="text-xl font-semibold">$100.8</div>
         </div>
       </div>
     </div>
